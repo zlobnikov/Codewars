1decbe869e336eeef69586c6f1d05943916ef85c
zlobnikov <daffodil@list.ru>
2021-06-04 08:42:55 +0300

    Extract the domain name from a URL

---
+++ 5kyu-extractDomainName.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Write a function that when given a URL as a string, parses out just the 
+// domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet"
+
+
+function domainName(url) {
+    url = url.replace(/https?:\/\/|www\./g, '');
+    return url.split('.')[0];
+}
